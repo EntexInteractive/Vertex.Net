8de12d2e98a7a84e973ae13f33fa1260f5b3ba2d
Kyle Ebbinga <26614720+TheGuitarleader@users.noreply.github.com>
2026-08-16 17:10:26 -0500

    Initial commit

---
+++ Interfaces/ServerInterface.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2026 Entex Interactive
+
+namespace Vertex.Net.Interfaces
+{
+    public sealed class ServerInterface(string url)
+    {
+        private readonly HttpClient _client = new();
+        private readonly string _url = url;
+
+        public async Task<string> GetVersionAsync()
+        {
+            using HttpRequestMessage request = new(HttpMethod.Get, $"{_url}/api/v1/server/version");
+            using HttpResponseMessage response = await _client.SendAsync(request);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ Responses/VersionResponse.cs
@@ -0,0 +1,9 @@
+﻿// Copyright 2026 Entex Interactive
+
+namespace Vertex.Net.Responses
+{
+    public class VersionResponse
+    {
+        
+    }
+}
